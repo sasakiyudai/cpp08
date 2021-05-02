285997e2b285033d45235d3d98270e55bdff9325
Your Name <you@example.com>
2021-05-02 14:33:30 +0900

    ex01

---
+++ ex01/span.cpp
@@ -0,0 +1,67 @@
+#include "span.hpp"
+#include <vector>
+#include <algorithm>
+
+Span::Span(unsigned int n):n(n)
+{
+
+}
+
+Span::Span(Span const &other)
+{
+	*this = other;
+}
+
+Span::~Span()
+{
+
+}
+
+Span &Span::operator=(Span const &other)
+{
+	n = other.n;
+	vector = other.vector;
+	return (*this);
+}
+
+void Span::addNumber(int num)
+{
+	if (vector.size() == (unsigned long)n)
+		throw OutLimitsException();
+	vector.push_back(num);
+}
+
+long Span::shortestSpan()
+{
+	if (vector.size() <= 1)
+		throw NoSpanException();
+	
+	std::vector<int> sorted_vector(vector);
+	std::sort(sorted_vector.begin(), sorted_vector.end());
+	
+	long ret = (long)sorted_vector[1] - (long)sorted_vector[0];
+	for (unsigned long i = 1; i < sorted_vector.size()-1; i++)
+		ret = std::min(ret, (long)sorted_vector[i+1]-(long)sorted_vector[i]);
+	return (ret);
+}
+
+long Span::longestSpan()
+{
+	if (vector.size() <= 1)
+		throw NoSpanException();
+	
+	long max = *std::max_element(vector.begin(), vector.end());
+	long min = *std::min_element(vector.begin(), vector.end());
+
+	return (max - min);
+}
+
+const char* Span::OutLimitsException::what() const throw()
+{
+	return "there is no enough space.";
+}
+
+const char* Span::NoSpanException::what() const throw()
+{
+	return "ther is no span.";
+}
---
+++ ex01/span.hpp
@@ -0,0 +1,35 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+
+class Span
+{
+	public:
+		Span(unsigned int n);
+		Span(Span const &other);
+		virtual ~Span();
+		Span &operator=(Span const &other);
+
+		void addNumber(int num);
+		template<class T>
+		void addNumber(T begin, T end)
+		{
+			if (vector.size() + std::distance(begin, end) > (unsigned long)n)
+				throw OutLimitsException();
+			vector.insert(vector.end(), begin, end);
+		}
+		long shortestSpan();
+		long longestSpan();
+		class OutLimitsException: public std::exception {
+			const char* what() const throw();
+		};
+		class NoSpanException: public std::exception {
+			const char* what() const throw();
+		};
+	private:
+		int n;
+		std::vector<int> vector;
+};
+
+#endif
